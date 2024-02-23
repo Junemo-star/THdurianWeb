de329ea65611e6bc9302eb1fccb074f0351450ca
Isara <6610110386@psu.ac.th>
2024-02-23 12:54:04 +0700

    แก้error ข้อมูล

--- clint/src/page/Delivery.js
+++ clint/src/page/Delivery.js
@@ -1,52 +1,25 @@
-<<<<<<< HEAD
-import React from "react"
-import styles from '../css/CssDelivery.module.css'
-import Footers from "../componet/Footerbar"
-
-
-const Delivery =() => {
-    return (
-        <div className={styles.set_pos}>
-            <div className={styles.box}>
-                <div className={styles.text}>
-                    สถานะการจัดส่งสินค้า 
-                    <div className={styles.text2}>
-                        สวนนายดำ <br/>
-                        จำนวน : <br/>
-                        ราคา : <br/>
-                        สถานะ : <br/>
-                    </div>
-                    <div className={styles.inside_box}>
-                    </div>
-                </div>
-=======
 import React, { useEffect, useState } from "react";
 import styles from "../css/CssDelivery.module.css";
 import Footers from "../componet/Footerbar";
+import { useAuth } from '../componet/AuthContext';
 import axios from "axios";
 
 function Delivery() {
   const [orders, setOrders] = useState([]);
+  const { token } = useAuth(); // Get the JWT token from the AuthContext
+
+  const fetchData = async () => {
+    try {
+      const ordersResponse = await axios.get("http://localhost:1337/api/placed-orders", token)
+      setOrders(ordersResponse.data.data)
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+    }
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const token = "your_jwt_token";
-
-        const ordersResponse = await axios.get("http://localhost:1337/api/placed-orders", {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-
-        setOrders(ordersResponse.data.data);
-      } catch (error) {
-        console.error("Error fetching data: ", error);
-      }
-    };
-
     fetchData();
-  }, []);
+  }, []); // Include authToken in the dependency array
 
   return (
     <div className={styles.set_pos}>
@@ -64,7 +37,6 @@ function Delivery() {
                 <br />
                 สถานะ : {order.attributes.status}
               </div>
->>>>>>> 8142e39964e271fd9fc108b157947d65fda2cf2c
             </div>
           ))}
         </div>
