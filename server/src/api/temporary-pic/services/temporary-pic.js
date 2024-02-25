d23f3a0c286950d5258052ad367226635f9f3afc
ศรัณยพงศ์ เอี่ยมอนงค์ <saranyapong272547@gmail.com>
2024-02-25 22:44:27 +0700

    ทำ footer สำหรับหน้า home ( pc)

--- server/src/api/temporary-pic/services/temporary-pic.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * temporary-pic service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::temporary-pic.temporary-pic');
