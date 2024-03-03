80dce954e6bbd90a41af386a2df3d4eaec51d304
Sarankorn <lome74269@gmail.com>
2024-03-03 22:08:29 +0700

    public get promotion with /api/news-promotions
    Always return the auto and active. no need to filter on client.

--- server/src/api/news-promotion/controllers/news-promotion.js
+++ server/src/api/news-promotion/controllers/news-promotion.js
@@ -7,5 +7,51 @@
 const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::news-promotion.news-promotion',({ strapi }) => ({
+    async find(ctx) {
+        const currDate = new Date();
+        console.log(currDate)
+        let entries = await strapi.db.query('api::news-promotion.news-promotion').findMany({
+            where: {
+                $or: [
+                    {
+                        activation:'Active',
+                    },
+                    {
+                        $and:[
+                            {
+                                activation:'Auto',
+                            },
+                            {
+                                startDate: {$lt: currDate}
+                            },
+                            {
+                                endDate: {$gt: currDate}
+                            },
+                        ]
+                    }
+
+                ],
+            },
+            populate: {
+                picture: true,
+
+            },
+        });
+        //console.log(entries);
+        return entries;
+    },
+    async adminfind(ctx) {
+        const currDate = new Date();
+        console.log(currDate)
+        let entries = await strapi.db.query('api::news-promotion.news-promotion').findMany({
+
+            populate: {
+                picture: true,
+
+            },
+        });
+        //console.log(entries);
+        return entries;
+    },
 
 }));
